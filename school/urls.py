5a04f91f4c17c85480435b5115100cfdaf00f78c
Jawad Hussain <jawadduet@gmail.com>
2024-07-24 09:55:05 +0500

    CRUD using APIView

--- school/urls.py
+++ school/urls.py
@@ -1,10 +1,19 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+# from .views import StudentViewSet
 
-router = DefaultRouter()
-router.register(r'students', StudentViewSet)
+# router = DefaultRouter()
+# router.register(r'students', StudentViewSet)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+# urls.py
+from django.urls import path
+from .views import StudentListCreateAPIView, StudentRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('students/', StudentListCreateAPIView.as_view(), name='student-list-create'),
+    path('students/<uuid:pk>/', StudentRetrieveUpdateDestroyAPIView.as_view(), name='student-detail'),
 ]
--- school/views.py
+++ school/views.py
@@ -1,7 +1,67 @@
-from rest_framework import viewsets
+# from rest_framework import viewsets
+# from .models import Student
+# from .serializers import StudentSerializer
+
+# class StudentViewSet(viewsets.ModelViewSet):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# views.py
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Student
 from .serializers import StudentSerializer
 
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+class StudentListCreateAPIView(APIView):
+    def get(self, request, format=None):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class StudentRetrieveUpdateDestroyAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return None
+
+    def get(self, request, pk, format=None):
+        student = self.get_object(pk)
+        if student is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        student = self.get_object(pk)
+        if student is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        student = self.get_object(pk)
+        if student is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def patch(self, request, pk, format=None):
+        student = self.get_object(pk)
+        if student is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = StudentSerializer(student, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
