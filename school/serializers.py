870d70e8eea321e63e3ccfac90ba1d708f958dc6
Jawad Hussain <jawadduet@gmail.com>
2024-07-24 09:43:11 +0500

    CRUD using model viewset

--- school/serializers.py
+++ school/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['name', 'age', 'bio']
+        fields = ['student_id','name', 'age','enrollment_date', 'bio']
